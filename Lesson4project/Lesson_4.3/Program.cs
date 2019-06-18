ed01d787443097cd039168ce79655e23bccb7f16
Maxyber <ghostpaladin@mail.ru>
2019-06-18 16:43:14 +0300

    Задание 4.3.1. выполнено, осталось 4.3.2 и все ДЗ готово.

---
+++ Lesson4project/Lesson_4.3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                {"four",4 },
+                {"two",2 },
+                { "one",1 },
+                {"three",3 },
+            };
+            #region Базовое задание
+            var d = dict.OrderBy(delegate (KeyValuePair<string, int> pair) { return pair.Value; });
+            foreach (var pair in d)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+            #endregion
+
+            Console.WriteLine("--- --- ---");
+            Console.ReadKey();
+
+            #region 4.3.1. свернуть OrderBy с помощью лямбда-выражений
+            d = dict.OrderBy(pair => pair.Value);
+            foreach (var pair in d)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+
+            #endregion
+
+            Console.WriteLine("--- --- ---");
+            Console.ReadKey();
+
+            #region 4.3.2. развернуть OrderBy с использованием делегата
+            d = dict.OrderBy(pair => pair.Value);
+            foreach (var pair in d)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+            #endregion
+
+        }
+    }
+}
